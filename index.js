f7810862f16ff36f0d11ac8ea3d90c38d959077e
uufuyfyolug <153218036+uufuyfyolug@users.noreply.github.com>
2024-10-30 10:10:02 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-let mudaCor = document.getElementById('um');
-mudaCor.addEventListener('click', function(){
-    um.style.backgroundcolor ='purple'
+let corfixa = document.getElementById('um');
+let corAtual = 'purple'
+var cores = ['green', 'yellow']
+let mudaCor = document.getElementById('dois', 'tres')
+corfixa.addEventListener('click', function(){
+    let mudaCor = document.getElementById('dois')
+    um.style.backgroundColor =corAtual
+    dois.style.backgroundColor =cores[1]? cores[0] : cores[1]
+    tres.style.backgroundColor = cores[0]? cores[1] : cores[0]
 })
